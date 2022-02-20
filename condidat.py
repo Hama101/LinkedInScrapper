92ac75e11454b61de7f11bacdc4ce1edfd833960
HamdiAAA <hamdi_mohamed2018@hotmail.com>
2022-02-19 21:36:42 -0400

    jsoning

--- condidat.py
+++ condidat.py
@@ -1,4 +1,4 @@
-from const import DRIVER , download_image , to_json , to_csv , flatten_list , login
+from const import DRIVER, download_image, to_json, to_csv, flatten_list, login
 from selenium.webdriver.common.keys import Keys
 import os
 import time
@@ -8,25 +8,26 @@ driver = DRIVER
 
 # a class to get data from a profile
 class Condidat:
-    def __init__(self, PROFILE_URL , driver , works_for):
+    def __init__(self, PROFILE_URL, driver, works_for):
         self.PROFILE_URL = PROFILE_URL
         self.driver = driver
         self.driver.get(PROFILE_URL)
         self.works_for = works_for
         time.sleep(3)
-    
-    
+
     def get_data(self):
-        self.full_name = self.driver.find_element_by_class_name('text-heading-xlarge').get_attribute('innerHTML')
-        self.img_url = self.driver.find_element_by_class_name('pv-top-card-profile-picture__image').get_attribute('src')
-        download_image(f"images/{self.works_for}/",self.img_url , f"{self.full_name}.jpg")
-        self.job = self.driver.find_element_by_class_name('text-body-medium').get_attribute('innerHTML')
-        
-        #get the code sr of the page than save it to html file
-        code_sr = self.driver.page_source
-        with open(f"html_files/{self.works_for}/{self.full_name}.html" , 'w' , encoding='utf-8') as f:
-            f.write(code_sr)
-        
+        self.full_name = self.driver.find_element_by_class_name(
+            "text-heading-xlarge"
+        ).get_attribute("innerHTML")
+        self.img_url = self.driver.find_element_by_class_name(
+            "pv-top-card-profile-picture__image"
+        ).get_attribute("src")
+        download_image(
+            f"images/{self.works_for}/", self.img_url, f"{self.full_name}.jpg"
+        )
+        self.job = self.driver.find_element_by_class_name(
+            "text-body-medium"
+        ).get_attribute("innerHTML")
 
         data = {
             "full_name": self.full_name,
@@ -34,53 +35,77 @@ class Condidat:
             "img_url": self.img_url,
             "job": self.job,
         }
-        #save the data to json file using json mode
+        # get the code sr of the page than save it to html file
+        code_sr = self.driver.page_source
+        # create a folder to save the html file
+        if not os.path.exists(f"html_files/{self.works_for}"):
+            os.makedirs(f"html/{self.works_for}")
+        if not os.path.exists(f"json_files/{self.works_for}"):
+            os.makedirs(f"html/{self.works_for}")
+        # save the html file
+        with open(
+            f"html_files/{self.works_for}/{self.full_name}.html", "w", encoding="utf-8"
+        ) as f:
+            f.write(code_sr)
+            # save it 's data to a json file
+        with open(
+            f"json_files/{self.works_for}/{self.full_name}.json", "w", encoding="utf-8"
+        ) as f:
+            json.dump(data, f, indent=4)
+
+        # save the data to json file using json mode
         return data
 
 
-
-
-#loop throw a list and iniate it condidat object than get data and append it to a new list
-def get_data(profiles , works_for = None):
+# loop throw a list and iniate it condidat object than get data and append it to a new list
+def get_data(profiles, works_for=None):
     data = []
     for profile in profiles:
-        condidat = Condidat(PROFILE_URL=profile , driver=driver , works_for=works_for)
+        condidat = Condidat(PROFILE_URL=profile, driver=driver, works_for=works_for)
         try:
             c_data = condidat.get_data()
-            #save the data to json file using json model
+            # save the data to json file using json model
         except Exception as e:
-            print("bad profile ",profile)
+            print("bad profile ", profile)
+            print(e)
             continue
-        print("got ----> ",c_data)
+        print("got ----> ", c_data)
         data.append(c_data)
     return data
 
 
-#the main function
+# the main function
 def main():
     profiles = []
     time.sleep(3)
-    for i in range(0,25):
-        #https://www.linkedin.com/search/results/people/?keywords=tunisia&origin=CLUSTER_EXPANSION&page=3&sid=-w)
-        #scrooll to the bottom of the page
+    for i in range(0, 25):
+        # https://www.linkedin.com/search/results/people/?keywords=tunisia&origin=CLUSTER_EXPANSION&page=3&sid=-w)
+        # scrooll to the bottom of the page
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        profiles.append([x.get_attribute('href') for x in driver.find_elements_by_class_name('app-aware-link')])
-        #time.sleep(2)
-        driver.get(f"https://www.linkedin.com/search/results/people/?keywords=tunisia&origin=CLUSTER_EXPANSION&page={i+1}&sid=-w)")
-        #time.sleep(2)
+        profiles.append(
+            [
+                x.get_attribute("href")
+                for x in driver.find_elements_by_class_name("app-aware-link")
+            ]
+        )
+        # time.sleep(2)
+        driver.get(
+            f"https://www.linkedin.com/search/results/people/?keywords=tunisia&origin=CLUSTER_EXPANSION&page={i+1}&sid=-w)"
+        )
+        # time.sleep(2)
         print(f"page {i}")
-    
+
     profiles = flatten_list(profiles)
-    print("profiles ",len(profiles))
-    print("profiles ",profiles[0])
-    #input("press enter to continue")
+    print("profiles ", len(profiles))
+    print("profiles ", profiles[0])
+    # input("press enter to continue")
     data = get_data(list(set(profiles)))
-    #to_json(data)
+    # to_json(data)
     to_csv(data)
     # create_json(data)
     driver.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     login()
     main()
--- start_up.py
+++ start_up.py
@@ -1,124 +1,148 @@
-from const import DRIVER , download_image , to_json , to_csv , flatten_list , login
+from const import DRIVER, download_image, to_json, to_csv, flatten_list, login
 from selenium.webdriver.common.keys import Keys
 import json
 import os
 import time
 import random
-from condidat import Condidat , get_data
+from condidat import Condidat, get_data
 
 driver = DRIVER
 
 
 class StartUp:
-    def __init__(self, PROFILE_URL , driver):
+    def __init__(self, PROFILE_URL, driver):
         self.PROFILE_URL = PROFILE_URL
         self.driver = driver
         self.driver.get(PROFILE_URL)
         time.sleep(10)
-    
+
     def get_employess(self):
-        url = self.driver.find_element_by_class_name('mt1').find_element_by_tag_name('a').get_attribute('href')
+        url = (
+            self.driver.find_element_by_class_name("mt1")
+            .find_element_by_tag_name("a")
+            .get_attribute("href")
+        )
+        str_nb_employees = str(
+            self.driver.find_element_by_class_name("mt1")
+            .find_element_by_tag_name("a")
+            .find_element_by_tag_name("span")
+            .get_attribute("innerHTML")
+        )
+        nb_employees = int("".join(char for char in str_nb_employees if char.isdigit()))
+        print(f"{self.full_name} has {nb_employees} employees")
         self.driver.get(url)
         time.sleep(2)
         n_url = self.driver.current_url
         employees = []
-        for i in range(0,30):
-            try:        
-                #click on the button and save the url
+        for i in range(0, 1):
+            try:
+                # click on the button and save the url
                 time.sleep(2)
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(2)
-                l = [x.get_attribute('href') for x in driver.find_elements_by_class_name('app-aware-link')]
+                l = [
+                    x.get_attribute("href")
+                    for x in driver.find_elements_by_class_name("app-aware-link")
+                ]
                 employees.append(l)
                 driver.get(f"{n_url}&page={i+1}")
             except Exception as e:
                 print(e)
                 break
 
-        #romve the duplicates from the list
+        # romve the duplicates from the list
         employees = list(set(flatten_list(employees)))
-        #getting the data from the profiles
-        data = get_data(profiles=employees , works_for=self.full_name)
+        # getting the data from the profiles
+        data = get_data(profiles=employees, works_for=self.full_name)
         return data
-    
+
     def get_data(self):
-        self.full_name = self.driver.find_element_by_class_name('t-24').find_element_by_tag_name('span').get_attribute('innerHTML')
-        self.img_url = self.driver.find_element_by_class_name('org-top-card-primary-content__logo').get_attribute('src')
-        download_image("images/startups/",self.img_url , f"{self.full_name}.jpg")
+        self.full_name = (
+            self.driver.find_element_by_class_name("t-24")
+            .find_element_by_tag_name("span")
+            .get_attribute("innerHTML")
+        )
+        self.img_url = self.driver.find_element_by_class_name(
+            "org-top-card-primary-content__logo"
+        ).get_attribute("src")
+        download_image("images/startups/", self.img_url, f"{self.full_name}.jpg")
         try:
-            self.bio = self.driver.find_element_by_class_name('org-top-card-summary__tagline').get_attribute('innerHTML')
+            self.bio = self.driver.find_element_by_class_name(
+                "org-top-card-summary__tagline"
+            ).get_attribute("innerHTML")
         except Exception as e:
             self.bio = ""
 
-        #get the code sr of the page than save it to html file
+        # get the code sr of the page than save it to html file
         code_sr = self.driver.page_source
-        with open(f"html_files/startups/{self.full_name}.html" , 'w' , encoding='utf-8') as f:
+        with open(
+            f"html_files/startups/{self.full_name}.html", "w", encoding="utf-8"
+        ) as f:
             f.write(code_sr)
-        
+
         data = {
             "full_name": self.full_name,
             "img_url": self.img_url,
             "bio": self.bio,
-            "employees": self.get_employess()
+            "employees": self.get_employess(),
         }
-        #save the data to json file using json model
-        with open(f"json_files/startups/{self.full_name}.json" , 'w' , encoding='utf-8') as f:
-            json.dump(data , f , indent=4)
-        
+        # save the data to json file using json model
+        with open(
+            f"json_files/startups/{self.full_name}.json", "w", encoding="utf-8"
+        ) as f:
+            json.dump(data, f, indent=4)
+
         return data
 
 
 def take_a_break():
     print("taking a break")
-    time.sleep(random.randint(5,10))
+    time.sleep(random.randint(5, 10))
     driver.get("https://www.linkedin.com/feed/")
-    #scroll throw the feed slowly
-    for i in range(0,10):
+    # scroll throw the feed slowly
+    for i in range(0, 10):
         time.sleep(1)
         driver.execute_script(f"window.scrollTo(0, {i*1000});")
-        likes_btn = driver.find_elements_by_class_name('reactions-react-button')
-        #get a sample from the likes_btn
+        likes_btn = driver.find_elements_by_class_name("reactions-react-button")
+        # get a sample from the likes_btn
         if likes_btn:
-            likes_btn[random.randint(0,len(likes_btn)-1)].click()
+            likes_btn[random.randint(0, len(likes_btn) - 1)].click()
             time.sleep(1)
-            likes_btn[random.randint(0,len(likes_btn)-1)].click()
+            likes_btn[random.randint(0, len(likes_btn) - 1)].click()
             time.sleep(2)
-            likes_btn[random.randint(0,len(likes_btn)-1)].click()
+            likes_btn[random.randint(0, len(likes_btn) - 1)].click()
             time.sleep(2)
-            likes_btn[random.randint(0,len(likes_btn)-1)].click()
+            likes_btn[random.randint(0, len(likes_btn) - 1)].click()
             time.sleep(2)
         else:
             continue
         time.sleep(1)
 
 
-
-#loop throw a list and iniate it condidat object than get data and append it to a new list
+# loop throw a list and iniate it condidat object than get data and append it to a new list
 def s_get_data(profiles):
     data = []
-    for index , profile in enumerate(profiles):
-        start_up = StartUp(PROFILE_URL=profile , driver=driver)
+    for index, profile in enumerate(profiles):
+        start_up = StartUp(PROFILE_URL=profile, driver=driver)
         try:
             c_data = start_up.get_data()
         except Exception as e:
             print(e)
             continue
-        print("got ----> ",c_data)
+        print("got ----> ", c_data)
         data.append(c_data)
         if index == 10:
             take_a_break()
     return data
 
 
-
-
 def main():
     profiles = []
-    with open('startups.txt' , 'r') as f:
+    with open("startups.txt", "r") as f:
         for line in f:
             profiles.append(line.strip())
-    print("profiles---> ",profiles)
-    #input("press enter to continue")
+    print("profiles---> ", profiles)
+    # input("press enter to continue")
     data = s_get_data(list(set(profiles)))
     driver.close()
 
